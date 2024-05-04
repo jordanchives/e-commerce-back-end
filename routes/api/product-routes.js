9a484707a6553f40f99471e8f52513a2f90e77ea
Jordan M <jormmiller@gmail.com>
2024-05-04 15:55:05 -0400

    created product and tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,8 +23,11 @@ router.get('/:id', async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
     res.status(200).json(productData);
-
   } catch (err) {
     res.status(500).json(err);
   }
